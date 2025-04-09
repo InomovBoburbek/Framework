287485fe597871f171a58052297b734ad12539e2
bobur <valiyev7115@gmail.com>
2025-04-09 14:29:02 +0500

    u

--- app.py
+++ app.py
@@ -11,35 +11,46 @@ class Frameworkapp:
 
     def handle_request(self, req):
         res = Response()
-        path_parts = req.path.strip("/").split("/")
+        is_found = False
 
         for path, handler in self.routes.items():
             if path == req.path:
                 handler(req, res)
+                is_found = True
             else:
                 parsed = parse(path, req.path)
 
+
                 if parsed is not None:
                     handler(req, res, parsed.named.get("id", "Bunday user yo'q!"))
+                    is_found = True
 
-            # /u/<id>
-            if path == "/u/id" and len(path_parts) == 2 and path_parts[0] == "u":
-                handler(req, res, path_parts[1])
-                return res
+        if not is_found:
+            self.default_response(res)
 
-            # /admin/<id>
-            elif path == "/admin/id" and len(path_parts) == 2 and path_parts[0] == "admin":
-                handler(req, res, path_parts[1])
-                return res
-
-            # static route
-            elif path == req.path:
-                handler(req, res)
-                return res
-
-        res.status = '404 Not Found'
-        res.text = 'Not Found'
         return res
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found!"
+            # # /u/<id>
+            # if path == "/u/id" and len(path_parts) == 2 and path_parts[0] == "u":
+            #     handler(req, res, path_parts[1])
+            #     return res
+            #
+            # # /admin/<id>
+            # elif path == "/admin/id" and len(path_parts) == 2 and path_parts[0] == "admin":
+            #     handler(req, res, path_parts[1])
+            #     return res
+            #
+            # # static route
+            # elif path == req.path:
+            #     handler(req, res)
+            #     return res
+
+        # res.status = '404 Not Found'
+        # res.text = 'Not Found'
+        # return res
 
     def route(self, path):
         def wrapper(handler):
